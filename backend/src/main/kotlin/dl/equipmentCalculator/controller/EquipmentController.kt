4db8797ee2e140b40bed8689e505e9b5fec16217
Sören Dierkes <soeren.dierkes@openknowledge.de>
2025-08-16 01:19:27 +0200

    Add api

--- backend/src/main/kotlin/dl/equipmentCalculator/controller/EquipmentController.kt
+++ backend/src/main/kotlin/dl/equipmentCalculator/controller/EquipmentController.kt
@@ -21,12 +21,14 @@ import org.springframework.cache.annotation.Cacheable
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ResponseStatusException
 
 @CrossOrigin
 @RestController
+@RequestMapping("/api")
 class EquipmentController {
 
     @Autowired
